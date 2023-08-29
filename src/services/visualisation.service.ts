903767df6fc62aea3b57db51d8a385c781cd6e3e
AnuragKumarChoudhary <anuragkumarchoudhary@outlook.com>
2023-08-30 01:50:15 +0530

    Added Link that will be dynamically generated

--- src/services/visualisation.service.ts
+++ src/services/visualisation.service.ts
@@ -242,8 +242,16 @@ export class VisualisationService {
       {
         "source": "ldm-a-r",
         "target": "du-if-d"
+      },
+      {
+        "source": "ldm-a-a",
+        "target": "du-if-d2"
       }
     ]
+
+    document.querySelectorAll("svg").forEach((e: any) => {
+      e.parentElement.removeChild(e);
+    })
 
     let conatinerWidth: number = document.querySelector(selector)!.getBoundingClientRect().width,
       containerHeight: number = document.querySelector(selector)!.getBoundingClientRect().height;
@@ -274,8 +282,6 @@ export class VisualisationService {
         .attr("y", (nodedata[i]["level"][1] * 400))
         .style("overflow", "visible")
         .html(() => {
-          console.log(nodedata[i]);
-
           let nodeHTML = `
           <div class="node-container">
           <div class="node-title"><span>${nodedata[i]["title"]}</span></div>
@@ -289,7 +295,35 @@ export class VisualisationService {
           nodeHTML += `</div></div></div>`
           return nodeHTML;
         })
+    }
 
+    for (let i = 0; i < linkData.length; i++) {
+      console.log(linkData[i]);
+      let source = document.querySelector(`#${linkData[i]["source"]}`)?.parentElement!.getBoundingClientRect();
+      let target = document.querySelector(`#${linkData[i]["target"]}`)?.parentElement!.getBoundingClientRect();
+      console.log(source, target);
+      let link = g.append("path")
+        .attr("stroke", "white")
+        .attr("stroke-width", "2px")
+        .attr("fill", "none")
+        .attr("d", () => {
+          // let path = `M ${paths.sources[i].x + paths.sources[i].width} ${paths.sources[i].y + (paths.sources[i].height / 2)} L${paths.sources[i].x + paths.sources[i].width + 50 - (paths.sources[i].index * 10)} ${paths.sources[i].y + (paths.sources[i].height / 2)} V${paths.sources[i].y + (paths.sources[i].height / 2)} ${paths.targets[i].y + (paths.targets[i].height / 2)} L${paths.targets[i].x - 300} ${paths.targets[i].y + (paths.targets[i].height / 2)}`;
+          let path = `M ${source!.x + source!.width - 30} ${source!.y - 10} L${target!.x - ((target!.x - source!.x) / 2)} ${source!.y - 10} V${source!.y - 10} ${target!.y - 10} L${target!.x - 30} ${target!.y - 10}`;
+          console.log(path);
+          return path;
+        })
+      svg.append("svg:defs").append("svg:marker")
+        .attr("id", "arrow")
+        .attr("viewBox", "0 -5 10 10")
+        .attr('refX', 10)
+        .attr("markerWidth", 5)
+        .attr("markerHeight", 5)
+        .attr("orient", "auto")
+        .append("svg:path")
+          .attr("d", "M0,-5L10,0L0,5")
+          .attr("fill", "white");
+
+      link.attr("marker-end", "url(#arrow)")
     }
 
 
